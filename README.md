7cf46ebb74447fe69a4d98fd7c21ce000769e2cb
Luke Davis <luke@peopleofthebit.com>
2014-04-01 14:12:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+protoarcane: a little experimental language that transpiles to javascript.
+
+inspired by arabic, protoarcane uses prefixes and suffixes to do programmy stuff.
+
+protoarcane is meant to be spoken, not typed, so special polysyllabic characters are replaced with root modifiers.
+
+all variable names should begin and end with vowels (since the prefixes and suffixes are all consonenty)
+ 
+try varash iriz irich in the text area on the right 
---
+++ js/appv0.0.2.js
@@ -0,0 +1,213 @@
+$(document).ready(function(){
+	app.c.init();
+	app.v.init();
+	app.c.listeners();
+})
+/////////////////////////////////////////////////////////////////////////////////
+
+var app={m:{},v:{},c:{}};
+
+/////////////////////////////////////////////////////////////////////////////////
+
+/*
+Notes on ROILA:
+
+a "bat"
+e "red"
+i "big"
+o "frost"
+u "but"
+
+b 	p
+f 	s
+l 	w
+m 	n
+
+j
+k 	
+t
+
+
+*/
+
+///////////////
+
+app.m.lexer=[
+	[/^v/gi,"var "],
+	[/^sp/gi,'function('],
+	[/^k/gi,"{"],
+	[/^b/gi,"["],
+	[/^p/gi,"("],
+	[/^d/gi,"."],
+	[/^l/gi," else"],
+	
+
+
+	[/sh$/gi,"=function(iri){return "],
+	[/n$/gi,'=function('],
+	[/th$/gi,'='],
+	[/r$/gi,'; '],
+	[/ch$/gi,";};"],
+	[/p$/gi,')'],
+	[/s$/gi,'()'],
+	[/k$/gi,'}'],
+	[/l$/gi,'<'],
+	[/g$/gi,'>'],
+	[/z$/gi,'*'],
+
+
+
+	//[/^f/gi,"for (var "],
+	[/imai/gi,';'],
+	[/apa/gi,'('],
+	[/opo/gi,')'],
+	[/aka/gi,'{'],
+	[/oko/gi,'}'],
+	[/aba/gi,'['],
+	[/obo/gi,']'],
+
+	[/ono/gi,"1"],
+	[/utu/gi,'2'],
+	[/ithri/gi,'3'],
+	[/oro/gi,'4'],
+	[/ivi/gi,'5'],
+	[/iksi/gi,'6'],
+	[/evi/gi,'7'],
+	[/aeti/gi,'8'],
+	[/ieni/gi,'9'],
+	[/iro/gi,'0'],
+	[/abo/gi,'+'],
+	[/aboba/gi,'++'],
+	[/imi/gi,'-'],
+	[/idvi/gi,'/'],
+	[/izi/gi,'*'],
+
+	[/ofra/gi,"for ("],
+	[/ifri/gi,"if ("],
+	[/elsi/gi,"else {"],
+	[/eru/gi,'return']
+];
+
+/////////////////////////////////////////////////////////////////////////////////
+
+app.c.init=function(){
+	app.m.metadata={"name":"Arcane","version":"0.0.2"};
+};
+
+app.c.listeners=function(){
+
+$("input#compile").click(function(){
+	var input=$("#arcane").val();
+	input=app.c.lexer(input);
+	$("div#compiled").html(input);
+});
+
+$("input#run").click(function(){
+	var input=$("#arcane").val();
+	input=app.c.lexer(input);
+	input=eval(input);
+	input=$("#codeToRun").val();
+	input=app.c.lexer(input);
+	input=eval(input);
+	$("div#output").html(input);
+});
+
+
+};
+
+app.c.lexer=function(input){
+	input=input.split(" ");
+	for (var i=0;i<input.length;i++){
+		for (var j=0;j<app.m.lexer.length;j++){
+			//console.log(app.m.lexer[j]);
+			//console.log(app.m.lexer[j][0].exec(input[i]));
+			input[i]=input[i].replace(app.m.lexer[j][0],app.m.lexer[j][1]);
+		}
+	}
+	input=input.join(" ");
+	return input;
+};
+
+///////////////////////////////////////////
+
+app.v.init=function(){
+	app.v.style();
+	var d="";
+	d+="<table width='100%' ><tr><td id='left'>";
+		d+="<textarea rows='5' cols='5' id='arcane' autofocus></textarea>";
+		d+="<input type='button' value='compile' id='compile'></input>";
+		d+="<div id='compiled'></div>";
+	d+="</td><td id='right'>";
+		d+="<textarea rows='5' cols='5' id='codeToRun' ></textarea>";
+		d+="<input type='button' value='run' id='run'></input>";
+		d+="<div id='output'></div>";
+	d+="</td></tr></table>";
+	$("body").html(d);
+};
+
+
+app.v.style=function(){
+	davis.style("body",{
+		"width":"100%",
+		"margin":"0px",
+		"padding":"0px",
+		"text-align":"center",
+		"background":"#555",
+		"color":"#fff",
+		"font-size":"3em"
+	});
+	davis.style("div",{
+		"padding":"0",
+		"font-size":"1.5em",
+		"margin":"30px"
+	});
+	davis.style("input[type=button]",{
+		"width":"100%",
+		"font-size":"2em"
+	});
+	davis.style("textarea",{
+		"width":"100%",
+		"font-size":"1.5em",
+		"margin":"0"
+	});
+	davis.style("table",{
+		"height":"100%",
+		"width":"100%",
+		"margin":"0",
+		"padding":"0",
+		"table-layout":"fixed"
+	});
+	davis.style("td",{
+		"padding":"20px",
+		"margin":"30px",
+		"vertical-align":"top",
+		"text-align":"center"
+	});
+	davis.style("td#left",{
+		"background":"#111",
+		"color":"#fff"
+	});
+	davis.style("td#right",{
+		"background":"#fff",
+		"color":"#000"
+	});
+	davis.style("td#right input",{
+		"background":"#000",
+		"color":"#fff"
+	});
+	davis.style("td#left input",{
+		"background":"#fff",
+		"color":"#000"
+	});
+
+	davis.style("td#right textarea",{
+		"background":"#000",
+		"color":"#fff"
+	});
+
+	davis.style("td#left textarea",{
+		"background":"#fff",
+		"color":"#000"
+	});
+
+};
